204bddab4b0ae16489d9cad3dda62b4d279430c5
gabrielle-leung <gabrielle.leung@outlook.com>
2022-04-08 20:07:01 +0200

    Ajout Xpath PageMachines & plan PMCreation

--- src/main/java/project/PageMachineCreation.java
+++ src/main/java/project/PageMachineCreation.java
@@ -1,5 +1,73 @@
 package project;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class PageMachineCreation {
+    
+
+    
+    /*
+    *TODO
+    *Accéder à la page de gestion des machines :
+    *Passer la souris sur l'onglet "Ressources" puis dans le sous-menu qui s'affiche, cliquer sur l'item "Machines".
+    */
+    public PageMachines clickEnregistrerMachine(WebDriver driver){
+        return null;
+}
+
+    public void remplirMachine(){
+
+    
+    }
+
+
+
+
+    /*
+    * PAS DE TEST 3
+    * Créer une machine - Accès au formulaire de création :
+    * Cliquer sur le bouton [Créer].
+    */
+
+    //Xpath du bouton Créer une machine
+    @FindBy(xpath = "")
+    private WebElement clickCreerMachine;
+
+    //Xpath de l'onglet "Données de la machine"
+    
+
+    /*
+    *Créer une machine - Conformité de l'onglet "Données de la machine" :
+    *Vérifier la conformité de l'onglet "Données de la machine".
+    */
+
+
+
+    /*
+    *TODO
+    *Créer une machine - Bouton [Sauver et continuer] :
+    *1 - Renseigner l'ensemble des champs du bloc "Données de base" avec les valeurs suivantes :
+    *- Code : décocher la case "Générer le code" et saisir la valeur suivante dans le champ de saisie : "MACHINETEST1"
+    *- Nom : MACHINETEST1
+    *- Description : MACHINETEST1
+    *- Type : laisser la valeur par défaut de la liste déroulante "Ressource normale"
+    *2 - Cliquer sur le bouton [Sauver et continuer].
+    */
+
+
+
+    /*
+    *TODO
+    *Retour page de gestion des machines :
+    *Cliquer sur le bouton [Annuler].
+    */
+
+    public PageMachines annuler(){
+        return null;
+    }
+
+
 
 }
--- src/main/java/project/PageMachines.java
+++ src/main/java/project/PageMachines.java
@@ -1,16 +1,61 @@
 package project;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class PageMachines extends Page {
 
+    //Xpath select menu Ressource-Machine
+    @FindBy(xpath = "//a[@href='/libreplan/resources/machine/machines.zul']")
+    private WebElement s_Machine;
 
-    /*
-    *TODO
-    *Cliquer sur le bouton [Créer].
-    */
-    public PageMachineCreation clickCreer(WebDriver driver) {
-        return null;
-    }
+    //Xpath de la colonne Nom
+    @FindBy(xpath = "//*[@id='pOCQs5-cave'][text()='Nom']")
+    private WebElement col_Machine;
+
+    //Xpath de la colonne Description
+    @FindBy(xpath = "//*[@id='pOCQt5-cave'][text()='Description']")
+    private WebElement col_Desc;
+
+    //Xpath de la colonne Code
+    @FindBy(xpath = "//*[@id='pOCQu5-cave'][text()='Code']")
+    private WebElement col_Code;
+
+    //Xpath de la colonne En file
+    @FindBy(xpath = "//*[@id='pOCQv5-cave'][text()='En file']")
+    private WebElement col_EnFile;
+
+    //Xpath de la colonne Opérations
+    @FindBy(xpath = "//*[@id='pOCQw5-cave'][text()='Opérations']")
+    private WebElement col_Operations;
+
+    //Xpath du champ de recherche "Filtré par" (champ de saisie + icône représentant une loupe)
+    @FindBy(xpath = "//*[@id='pOCQq4-real'][@class='z-bandbox-inp']")
+    private WebElement c_SaisieFiltre;
+
+    @FindBy(xpath = "//*[@id='pOCQq4-btn'][@class='z-bandbox-btn']")
+    private WebElement b_SaisieFiltre;
+    
+    //Xpath du champ "Détails personnels"
+    @FindBy(xpath = "//input[@id='pOCQd5']")
+    private WebElement c_DetailsPerso;
+
+    //Xpath du bouton bleu clair [Plus d'options]
+    @FindBy(xpath = "//*[@id='pOCQf5-cnt'][contains(., 'options')]")
+    private WebElement b_PlusOptions;
+
+    //Xpath du bouton [Filtre]
+    //*[@id="pOCQn5-box"]/tbody/tr[2]/td[@class="z-button-cm"]
+    @FindBy(xpath = "//*[@id='pOCQn5-box']/tbody/tr[2]/td[contains(., 'Filtre')]")
+    private WebElement b_Filtre;
+
+    //Xpath du bouton [Créer]
+    @FindBy(xpath = "//*[@id='lSGQx5-box']/tbody/tr[2]/td[contains(., 'Créer')]")
+    private WebElement b_Creer;
+
+
+
+
+    //Methodes
 
 }
