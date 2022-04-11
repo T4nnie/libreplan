d5287788d5c0b3c4a884fb97e3733690f62c0b3e
T4nnie <tannie.stovall@gmail.com>
2022-04-11 15:51:33 +0200

    Properties

--- src/test/java/project/PRO_TA.java
+++ src/test/java/project/PRO_TA.java
@@ -2,6 +2,12 @@ package project;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.imageio.stream.FileImageInputStream;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -19,6 +25,8 @@ import project.Outils.Debug;
 public class PRO_TA {
 
     public static WebDriver driver;
+    public static Properties identifiant;
+    public static Properties DATA;
 
     /* Debut de tout test de cette classe
     * Set le webdriver
@@ -27,7 +35,13 @@ public class PRO_TA {
     public static void set(){
         //System.setProperty("webdriver.chrome.driver","src/main/resources/drivers/chromedriver.exe");
         System.setProperty("webdriver.gecko.driver","src/main/resources/drivers/geckodriver.exe");
-        driver = new FirefoxDriver();       
+        driver = new FirefoxDriver();
+        try{
+        identifiant.load(new FileInputStream("src/main/resources/identifiant.properties"));       
+        DATA.load(new FileInputStream("src/main/resources/PRO_TA.properties"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }       
     }
     
     /* Debut de chaque test de cette class
@@ -50,10 +64,10 @@ public class PRO_TA {
     @Test
     public void PROTA_01(){
         PageLogin pl = PageFactory.initElements(driver, PageLogin.class);   
-        PageAccueil pa = pl.seConnecter(driver, "admin ", "admin");
+        PageAccueil pa = pl.seConnecter(driver, identifiant.getProperty("nomUtilisateur"),identifiant.getProperty("motDePasse"));
         PageCreationProjet fc = pa.clickCreerProjet(driver);
         //assert(pa.isMe());
-        Debug.getAllHandles(driver);
+        fc.remplirFormulaire(DATA.getProperty("nvProjet_nom"), DATA.getProperty("nvProjet_code"), "Date", "dateEcheance");
     
 
     }
